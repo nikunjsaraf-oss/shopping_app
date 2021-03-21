8c34a853fb900722fd6e1f85f5dba959e051cfc4
LAPTOP-0261QBGC\nikun <nikunjsaraf2001@gmail.com>
2021-03-21 20:50:32 +0530

    implemented product's detail page using provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeNamed = '/product-overview';
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
